7073c088687bf3312a6105e1c0528aa942db0de5
avinash <avinashp7588@gmail.com>
2022-11-04 11:42:43 +0530

    Updates

---
+++ precache-manifest.47c673059d06d7a3a4b587ffae744381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b438b0d20cc638fbc4978cff27c8c1",
+    "url": "avinashp9987.github.io/react_upload/index.html"
+  },
+  {
+    "revision": "0d833c1ad4861b069d7d",
+    "url": "avinashp9987.github.io/react_upload/static/css/main.9dff3173.chunk.css"
+  },
+  {
+    "revision": "b9d8a6cae08e95790b8c",
+    "url": "avinashp9987.github.io/react_upload/static/js/2.e4b9a0a1.chunk.js"
+  },
+  {
+    "revision": "d58fd5cbf27e41292f8c",
+    "url": "avinashp9987.github.io/react_upload/static/js/3.a1727640.chunk.js"
+  },
+  {
+    "revision": "0d833c1ad4861b069d7d",
+    "url": "avinashp9987.github.io/react_upload/static/js/main.50b8c4c7.chunk.js"
+  },
+  {
+    "revision": "3b718b50b17545aeca3c",
+    "url": "avinashp9987.github.io/react_upload/static/js/runtime~main.fc2819ce.js"
+  }
+]);
